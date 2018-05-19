8ea6980498b54e0fd2b77a6e325cb03d67ff4ff8
PrzemyslawKlys <pklys@evotec.pl>
2018-05-20 00:00:18 +0200

    New config file

--- PSADReport.ps1
+++ PSADReport.ps1
@@ -26,82 +26,140 @@ $FormattingParameters = @{
     FontHeadingSize   = "12pt"
 }
 $ReportOptions = @{
-    JustTestPrerequisite            = $false # runs testing without actually running script
-    OnlyPrimaryDC                   = $false # usually should query all DC's but for testing can query just one (PrimaryDC)
+    JustTestPrerequisite  = $false # runs testing without actually running script
+    IncludeTimeToGenerate = $false # report with time it takes to generate (need this for debugging)
 
-    IncludeTimeToGenerate           = $true # report with time it takes to generate (need this for debugging)
-    IncludeDomainControllers        = $true
-    IncludeGroupEvents              = $true
-    IncludeUserEvents               = $true
-    IncludeUserStatuses             = $true
-    IncludeUserLockouts             = $true
-    IncludeGroupCreateDelete        = $true
-    IncludeDomainControllersReboots = $false # A bit useless if I'm to be honest
-    IncludeLogonEvents              = $false # DO NOT USE - NOT FINISHED
-    IncludeGroupPolicyChanges       = $false # DO NOT USE - NOT FINISHED
-    IncludeClearedLogs              = $false # DO NOT USE - NOT FINISHED
-    IncludeEventLogSize             = @{
-        Use    = $true
-        Logs   = "Security"#, "Application"
-        SortBy = ""
-    }
-    # Report Per Hour
-    ReportPastHour                  = $false # if it's 23:22 it will report 22:00 till 23:00
-    ReportCurrentHour               = $false # if it's 23:22 it will report 23:00 till 00:00
-    # Report Per Day
-    ReportPastDay                   = $false # if it's 1.04.2018 it will report 31.03.2018 00:00:00 till 01.04.2018 00:00:00
-    ReportCurrentDay                = $false # if it's 1.04.2018 05:22 it will report 1.04.2018 00:00:00 till 01.04.2018 00:00:00
-    # Report Per Week
-    ReportOnDay                     = @{
-        Use  = $true
-        Days = '"Monday'#, 'Tuesday'
-    }
-    # Report Per Month
-    ReportPastMonth                 = @{
-        Use   = $true # checks for 1st day of the month - won't run on any other day unless used force
-        Force = $false  # if true - runs always ...
-    }
-    ReportCurrentMonth              = $false
+    AsExcel               = $true # attaches Excel to email with all events, required ImportExcel module
+    AsCSV                 = $false # attaches CSV to email with all events,
+    AsHTML                = $true # puts exported data into email directly with all events
+    SendMail              = $true
+    OpenAsFile            = $true
+    KeepReports           = $true # keeps files after reports are sent (only if AssExcel/AsCSV are in use)
+    KeepReportsPath       = "C:\Support\Reports\ExportedEvents" # if empty, temp path is used
+    FilePattern           = "Evotec-ADMonitoredEvents-<currentdate>.xlsx"
+    FilePatternDateFormat = "yyyy-MM-dd-HH_mm_ss"
 
-    # Report Per Quarter
-    ReportPastQuarter               = @{
-        Use   = $true # checks for 1st day fo the quarter - won't run on any other day
-        Force = $false
-    }
-    ReportCurrentQuarter            = $false
-    # Report Custom
-    ReportCurrentDayMinusDayX       = @{
-        Use  = $false
-        Days = 7    # goes back X days and shows just 1 day
-    }
-    ReportCurrentDayMinuxDaysX      = @{
-        Use  = $false
-        Days = 3 # goes back X days and shows X number of days till Today
-    }
-    ReportCustomDate                = @{
-        Use      = $false
-        DateFrom = get-date -Year 2018 -Month 03 -Day 19
-        DateTo   = get-date -Year 2018 -Month 03 -Day 23
-    }
-
-    # AsExcel requires Import-Module ImportExcel
-    AsExcel                         = $true # attaches Excel to email with all events
-    AsCSV                           = $false # attaches CSV to email with all events,
-    AsHTML                          = $true # puts exported data into email directly with all events
-    SendMail                        = $true
-    KeepReports                     = $true # keeps files after reports are sent (only if AssExcel/AsCSV are in use)
-    KeepReportsPath                 = "C:\Support\Reports\ExportedEvents" # if empty, temp path is used
-    FilePattern                     = "Evotec-ADMonitoredEvents-<currentdate>.xlsx"
-    FilePatternDateFormat           = "yyyy-MM-dd-HH_mm_ss"
-
-    DisplayConsole                  = @{
+    DisplayConsole        = @{
         ShowTime   = $true
         LogFile    = ""
         TimeFormat = "yyyy-MM-dd HH:mm:ss"
     }
 }
 
+$ReportTimes = @{
+    # Report Per Hour
+    PastHour             = $false # if it's 23:22 it will report 22:00 till 23:00
+    CurrentHour          = $false # if it's 23:22 it will report 23:00 till 00:00
+    # Report Per Day
+    PastDay              = $false # if it's 1.04.2018 it will report 31.03.2018 00:00:00 till 01.04.2018 00:00:00
+    CurrentDay           = $true # if it's 1.04.2018 05:22 it will report 1.04.2018 00:00:00 till 01.04.2018 00:00:00
+    # Report Per Week
+    OnDay                = @{
+        Enabled = $true
+        Days    = '"Monday'#, 'Tuesday'
+    }
+    # Report Per Month
+    PastMonth            = @{
+        Enabled = $false # checks for 1st day of the month - won't run on any other day unless used force
+        Force   = $false  # if true - runs always ...
+    }
+    CurrentMonth         = $false
+
+    # Report Per Quarter
+    PastQuarter          = @{
+        Enabled = $false # checks for 1st day fo the quarter - won't run on any other day
+        Force   = $false
+    }
+    CurrentQuarter       = $false
+    # Report Custom
+    CurrentDayMinusDayX  = @{
+        Enabled = $false
+        Days    = 7    # goes back X days and shows just 1 day
+    }
+    CurrentDayMinuxDaysX = @{
+        Enabled = $false
+        Days    = 3 # goes back X days and shows X number of days till Today
+    }
+    CustomDate           = @{
+        Enabled  = $false
+        DateFrom = get-date -Year 2018 -Month 03 -Day 19
+        DateTo   = get-date -Year 2018 -Month 03 -Day 23
+    }
+}
+
+$ReportDefinitions = @{
+    ReportsAD = @{
+        Servers    = @{
+            Automatic = $true
+            OnlyPDC   = $false
+            DC        = ''
+        }
+        EventBased = @{
+            UserChanges            = @{
+                Enabled = $false
+                Events  = 4720, 4738
+                LogName = 'Security'
+            }
+            UserStatus             = @{
+                Enabled = $false
+                Events  = 4722, 4725, 4767, 4723, 4724, 4726
+                LogName = 'Security'
+            }
+            UserLockouts           = @{
+                Enabled = $false
+                Events  = 4740
+                LogName = 'Security'
+            }
+            GroupMembershipChanges = @{
+                Enabled = $false
+                Events  = 4728, 4729, 4732, 4733, 4756, 4757, 4761, 4762
+                LogName = 'Security'
+            }
+            GroupCreateDelete      = @{
+                Enabled = $false
+                Events  = 4727, 4730, 4731, 4734, 4759, 4760, 4754, 4758
+                LogName = 'Security'
+            }
+            UserLogon              = @{
+                Enabled = $false
+                Events  = 4624
+                LogName = 'Security'
+            }
+            GroupPolicyChanges     = @{
+                Enabled = $false
+                Events  = 5136, 5137, 5141
+                LogName = 'Security'
+            }
+            LogsClearedSecurity    = @{
+                Enabled = $false
+                Events  = 1102
+                LogName = 'Security'
+            }
+            LogsClearedOther       = @{
+                Enabled = $false
+                Events  = 104
+                LogName = 'System' # Source: EventLog, Task: 'Log clear'
+            }
+            EventsReboots          = @{
+                Enabled = $false
+                Events  = 1001, 1018, 1, 12, 13, 42, 41, 109, 1, 6005, 6006, 6008, 6013
+                LogName = 'System'
+            }
+        }
+        Custom     = @{
+            EventLogSize = @{
+                Enabled = $true
+                Logs    = 'Security', 'Application', 'System'
+                SortBy  = ''
+            }
+            ServersData  = @{
+                Enabled = $true
+            }
+        }
+    }
+}
+
 ### Starts Module (Requires config above)
 Clear-Host
 Import-Module PSWinReporting -Force
-Start-ADReporting $EmailParameters $ReportOptions $FormattingParameters
+Start-ADReporting -EmailParameters $EmailParameters -FormattingParameters $FormattingParameters -ReportOptions $ReportOptions -ReportDefinitions $ReportDefinitions -ReportTimes $ReportTimes
