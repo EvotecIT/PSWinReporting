ddec67784bc6e2cc41474b14bcb15d83b55f191c
PrzemyslawKlys <pklys@evotec.pl>
2018-05-20 12:20:42 +0200

    New options

--- PSADReport.ps1
+++ PSADReport.ps1
@@ -96,54 +96,64 @@ $ReportDefinitions = @{
         }
         EventBased = @{
             UserChanges            = @{
-                Enabled = $false
-                Events  = 4720, 4738
-                LogName = 'Security'
+                Enabled     = $false
+                Events      = 4720, 4738
+                LogName     = 'Security'
+                IgnoreWords = ''
             }
             UserStatus             = @{
-                Enabled = $false
-                Events  = 4722, 4725, 4767, 4723, 4724, 4726
-                LogName = 'Security'
+                Enabled     = $false
+                Events      = 4722, 4725, 4767, 4723, 4724, 4726
+                LogName     = 'Security'
+                IgnoreWords = ''
             }
             UserLockouts           = @{
-                Enabled = $false
-                Events  = 4740
-                LogName = 'Security'
+                Enabled     = $false
+                Events      = 4740
+                LogName     = 'Security'
+                IgnoreWords = ''
             }
             GroupMembershipChanges = @{
-                Enabled = $false
-                Events  = 4728, 4729, 4732, 4733, 4756, 4757, 4761, 4762
-                LogName = 'Security'
+                Enabled     = $false
+                Events      = 4728, 4729, 4732, 4733, 4756, 4757, 4761, 4762
+                LogName     = 'Security'
+                IgnoreWords = ''
             }
             GroupCreateDelete      = @{
-                Enabled = $false
-                Events  = 4727, 4730, 4731, 4734, 4759, 4760, 4754, 4758
-                LogName = 'Security'
+                Enabled     = $false
+                Events      = 4727, 4730, 4731, 4734, 4759, 4760, 4754, 4758
+                LogName     = 'Security'
+                IgnoreWords = ''
             }
             UserLogon              = @{
-                Enabled = $false
-                Events  = 4624
-                LogName = 'Security'
+                Enabled     = $false
+                Events      = 4624
+                LogName     = 'Security'
+                IgnoreWords = ''
             }
             GroupPolicyChanges     = @{
-                Enabled = $false
-                Events  = 5136, 5137, 5141
-                LogName = 'Security'
+                Enabled     = $false
+                Events      = 5136, 5137, 5141
+                LogName     = 'Security'
+                IgnoreWords = ''
             }
             LogsClearedSecurity    = @{
-                Enabled = $false
-                Events  = 1102
-                LogName = 'Security'
+                Enabled     = $false
+                Events      = 1102
+                LogName     = 'Security'
+                IgnoreWords = ''
             }
             LogsClearedOther       = @{
-                Enabled = $false
-                Events  = 104
-                LogName = 'System' # Source: EventLog, Task: 'Log clear'
+                Enabled     = $false
+                Events      = 104
+                LogName     = 'System' # Source: EventLog, Task: 'Log clear'
+                IgnoreWords = ''
             }
             EventsReboots          = @{
-                Enabled = $false
-                Events  = 1001, 1018, 1, 12, 13, 42, 41, 109, 1, 6005, 6006, 6008, 6013
-                LogName = 'System'
+                Enabled     = $false
+                Events      = 1001, 1018, 1, 12, 13, 42, 41, 109, 1, 6005, 6006, 6008, 6013
+                LogName     = 'System'
+                IgnoreWords = ''
             }
         }
         Custom     = @{
@@ -162,4 +172,4 @@ $ReportDefinitions = @{
 ### Starts Module (Requires config above)
 Clear-Host
 Import-Module PSWinReporting -Force
-Start-ADReporting -EmailParameters $EmailParameters -FormattingParameters $FormattingParameters -ReportOptions $ReportOptions -ReportDefinitions $ReportDefinitions -ReportTimes $ReportTimes
+Start-ADReporting -EmailParameters $EmailParameters -FormattingParameters $FormattingParameters -ReportOptions $ReportOptions -ReportTimes $ReportTimes -ReportDefinitions $ReportDefinitions
