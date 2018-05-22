01915458cba974fc53b464494b05662e0a25ca86
PrzemyslawKlys <pklys@evotec.pl>
2018-05-22 21:44:18 +0200

    Small cleanup

--- PSWinReporting.psm1
+++ PSWinReporting.psm1
@@ -10,11 +10,5 @@ Foreach ($import in @($Public + $Private)) {
         Write-Error -Message "Failed to import function $($import.fullname): $_"
     }
 }
-
-# Here I might...
-# Read in or create an initial config file and variable
-# Export Public functions ($Public.BaseName) for WIP modules
-# Set variables visible to the module and its functions only
-
 #Export-ModuleMember -Function $Public.Basename
 Export-ModuleMember -Function 'Start-ADReporting'
