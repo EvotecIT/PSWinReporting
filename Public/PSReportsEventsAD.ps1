79a5b856394b4661f55241a38b26e5c2bafc987f
PrzemyslawKlys <pklys@evotec.pl>
2018-05-27 00:22:37 +0200

    Fixes to reports

--- Public/PSReportsEventsAD.ps1
+++ Public/PSReportsEventsAD.ps1
@@ -160,3 +160,41 @@ function Get-LogonEventsKerberos($Events) {
 
     return $EventsFound
 }
+function Get-RebootEvents($Events) {
+
+    Write-Color @script:WriteParameters "[i] Running ", "Reboot Events Report (Troubleshooting Only)." -Color White, Green, White, Green, White, Green, White
+
+    # -LogName "System" -Provider "User32"
+    # -LogName "System" -Provider "Microsoft-Windows-WER-SystemErrorReporting" -EventID 1001, 1018
+    # -LogName "System" -Provider "Microsoft-Windows-Kernel-General" -EventID 1, 12, 13
+    # -LogName "System" -Provider "Microsoft-Windows-Kernel-Power" -EventID 42, 41, 109
+    # -LogName "System" -Provider "Microsoft-Windows-Power-Troubleshooter" -EventID 1
+    # -LogName "System" -Provider "Eventlog" -EventID 6005, 6006, 6008, 6013
+
+    $EventsNeeded = 1001, 1018, 1, 12, 13, 42, 41, 109, 1, 6005, 6006, 6008, 6013 | Sort-Object -Unique
+    $EventsFound = Find-EventsNeeded -Events $Events -EventsNeeded $EventsNeeded -EventsType 'System'
+    Write-Color @script:WriteParameters "[i] Ending ", "Reboot Events Report (Troubleshooting Only)." -Color White, Green, White, Green, White, Green, White
+    return $EventsFound | Select-Object ID, Computer, TimeCreated, Message
+}
+
+function Get-EventLogClearedLogs($Events, $Type) {
+    if ($Type -eq 'Security') {
+        $EventsNeeded = 1102
+        $EventsFound = Find-EventsNeeded -Events $Events -EventsNeeded $EventsNeeded -EventsType 'Security'
+    } else {
+        $EventsNeeded = 104
+        $EventsFound = Find-EventsNeeded -Events $Events -EventsNeeded $EventsNeeded -EventsType 'System'
+    }
+    #return $EventsFound
+    $EventsFound = $EventsFound | Select-Object @{label = 'Domain Controller'; expression = { $_.Computer}} ,
+
+    @{label = 'Action'; expression = { ($_.Message -split '\n')[0] }},
+    BackupPath,
+    Channel,
+    @{label = 'Who'; expression = { "$($_.SubjectDomainName)\$($_.SubjectUserName)" }},
+    @{label = 'When'; expression = { $_.Date }},
+    @{label = 'Event ID'; expression = { $_.ID }},
+    @{label = 'Record ID'; expression = { $_.RecordId }} | Sort-Object When
+
+    return $EventsFound
+}
