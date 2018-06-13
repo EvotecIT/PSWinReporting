907b3482fa318543a06d028438954598022804e1
PrzemyslawKlys <pklys@evotec.pl>
2018-06-13 16:40:53 +0200

    Do not add worksheet if no events reported

--- Private/PSWinReportingHelper.ps1
+++ Private/PSWinReportingHelper.ps1
@@ -129,7 +129,7 @@ function Remove-ReportsFiles ($KeepReports, $AsExcel, $AsCSV, $ReportFiles) {
     }
 }
 function Export-ReportToXLSX ($Report, $ReportOptions, $ReportFilePath, $ReportName, $ReportTable) {
-    if ($Report -eq $true) {
+    if (($Report -eq $true) -and ($($ReportTable | Measure-Object).Count -gt 0)) {
         $ReportTable | Export-Excel -Path $ReportFilePath -WorkSheetname $ReportName -AutoSize -FreezeTopRow -AutoFilter
         return
     } else {
