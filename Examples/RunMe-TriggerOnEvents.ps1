997e4b846abf88bf362132369cb004a279ea00aa
PrzemyslawKlys <pklys@evotec.pl>
2018-07-11 13:21:51 +0200

    Updates

--- Examples/RunMe-TriggerOnEvents.ps1
+++ Examples/RunMe-TriggerOnEvents.ps1
@@ -10,7 +10,115 @@ Import-Module PSEventViewer -Force
 Import-Module PSWinReporting -Force
 Import-Module PSWriteColor -Force
 
-Write-Color 'Executed Trigger for ID: ', $eventid, ' and RecordID: ', $eventRecordID -LogFile 'C:\Log.txt'
+$ReportOptions = @{
+    JustTestPrerequisite  = $false # runs testing without actually running script
+
+    AsExcel               = $true # attaches Excel to email with all events, required ImportExcel module
+    AsCSV                 = $false # attaches CSV to email with all events,
+    AsHTML                = $true # puts exported data into email directly with all events
+    SendMail              = $false
+    OpenAsFile            = $true # requires AsHTML set to $true
+    KeepReports           = $true # keeps files after reports are sent (only if AssExcel/AsCSV are in use)
+    KeepReportsPath       = 'C:\Support\Reports\ExportedEvents' # if empty, temp path is used
+    FilePattern           = 'Evotec-ADMonitoredEvents-<currentdate>.<extension>'
+    FilePatternDateFormat = 'yyyy-MM-dd-HH_mm_ss'
+
+    DisplayConsole        = @{
+        ShowTime   = $true
+        LogFile    = ''
+        TimeFormat = 'yyyy-MM-dd HH:mm:ss'
+    }
+    Debug                 = @{
+        DisplayTemplateHTML = $false
+        Verbose             = $false
+    }
+}
+$ReportDefinitions = @{
+    TimeToGenerate = $false
+
+    ReportsAD      = @{
+        EventBased = @{
+            UserChanges            = @{
+                Enabled     = $true
+                Events      = 4720, 4738
+                LogName     = 'Security'
+                IgnoreWords = ''
+            }
+            UserStatus             = @{
+                Enabled     = $true
+                Events      = 4722, 4725, 4767, 4723, 4724, 4726
+                LogName     = 'Security'
+                IgnoreWords = @{
+                    'Domain Controller' = ''
+                    'Action'            = ''
+                    'User Affected'     = 'Win-*', '*AD1$*'
+                    'Who'               = ''
+                    'When'              = ''
+                    'Event ID'          = ''
+                    'Record ID'         = ''
+                }
+            }
+            UserLockouts           = @{
+                Enabled     = $true
+                Events      = 4740
+                LogName     = 'Security'
+                IgnoreWords = ''
+            }
+            UserLogon              = @{
+                Enabled     = $false
+                Events      = 4624
+                LogName     = 'Security'
+                IgnoreWords = ''
+            }
+            UserLogonKerberos      = @{
+                Enabled     = $false
+                Events      = 4768
+                LogName     = 'Security'
+                IgnoreWords = ''
+            }
+            GroupMembershipChanges = @{
+                Enabled     = $true
+                Events      = 4728, 4729, 4732, 4733, 4756, 4757, 4761, 4762
+                LogName     = 'Security'
+                IgnoreWords = @{
+                    'Who' = '*ANONYMOUS*'
+                }
+            }
+            GroupCreateDelete      = @{
+                Enabled     = $true
+                Events      = 4727, 4730, 4731, 4734, 4759, 4760, 4754, 4758
+                LogName     = 'Security'
+                IgnoreWords = @{
+                    'Who' = '*ANONYMOUS*'
+                }
+            }
+            GroupPolicyChanges     = @{
+                Enabled     = $false
+                Events      = 5136, 5137, 5141
+                LogName     = 'Security'
+                IgnoreWords = ''
+            }
+            LogsClearedSecurity    = @{
+                Enabled     = $true
+                Events      = 1102
+                LogName     = 'Security'
+                IgnoreWords = ''
+            }
+            LogsClearedOther       = @{
+                Enabled     = $true
+                Events      = 104
+                LogName     = 'System'
+                IgnoreWords = ''
+            }
+            EventsReboots          = @{
+                Enabled     = $false
+                Events      = 1001, 1018, 1, 12, 13, 42, 41, 109, 1, 6005, 6006, 6008, 6013
+                LogName     = 'System'
+                IgnoreWords = ''
+            }
+        }
+    }
+}
 
 $TeamsID = ''
-Start-TeamsReport -EventID $EventID -EventRecordID $EventRecordID -EventChannel $EventChannel -TeamsID $TeamsID
+Start-TeamsReport -ReportDefinitions $ReportDefinitions -ReportOptions $ReportOptions -EventID $EventID -EventRecordID $EventRecordID -EventChannel $EventChannel -TeamsID $TeamsID
