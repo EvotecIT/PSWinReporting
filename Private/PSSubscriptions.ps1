2bf117cb079c8b9ce067534cd6155203edaf0d95
PrzemyslawKlys <pklys@evotec.pl>
2018-07-11 18:56:44 +0200

    Update

--- Private/PSSubscriptions.ps1
+++ Private/PSSubscriptions.ps1
@@ -4,35 +4,40 @@ function New-SubscriptionTemplates {
     )
     $Events = Get-EventsData -ReportDefinitions $ReportDefinitions -LogName 'Security'
     $Systems = Get-EventsData -ReportDefinitions $ReportDefinitions -LogName 'System'
-
+    Write-Color 'Found Security Events ', ([string] $Events) -Color White, Yellow
+    Write-Color 'Found System Events ', ([string] $Systems) -Color White, Yellow
     $Servers = Find-ServersAD -ReportDefinitions $ReportDefinitions
-
-    $xmlTemplate = "$($(Get-Module -ListAvailable PSWinReporting).ModuleBase)\Templates\Template-Collector.xml"
-
-    $ListTemplates = New-ArrayList
+    Write-Color 'Found Servers ', ([string] $Servers) -Color White, Yellow
+    $xmlTemplate = "$($($(Get-Module -ListAvailable PSWinReporting)[0]).ModuleBase)\Templates\Template-Collector.xml"
     if (Test-Path $xmlTemplate) {
-        $Array = New-ArrayList
-        $SplitArrayID = Split-Array -inArray $Events -size 22  # Support for more ID's then 22 (limitation of Get-WinEvent)
-        foreach ($ID in $SplitArrayID) {
-            Add-ToArray -List $Array -Element (New-EventQuery -Events $ID -Type 'Security')
+        Write-Color 'Found Template ', $xmlTemplate -Color White, Yellow
+        $ListTemplates = New-ArrayList
+        if (Test-Path $xmlTemplate) {
+            $Array = New-ArrayList
+            $SplitArrayID = Split-Array -inArray $Events -size 22  # Support for more ID's then 22 (limitation of Get-WinEvent)
+            foreach ($ID in $SplitArrayID) {
+                Add-ToArray -List $Array -Element (New-EventQuery -Events $ID -Type 'Security')
+            }
+            Add-ToArray -List $Array -Element (New-EventQuery -Events $Systems -Type 'System')
+            $i = 0
+            foreach ($Events in $Array) {
+                $i++
+                $SubscriptionTemplate = "$ENV:TEMP\PSWinReportingSubscription$i.xml"
+                Copy-Item -Path $xmlTemplate $SubscriptionTemplate
+                Write-Color 'Copied template ', $SubscriptionTemplate -Color White, Yellow
+                Add-ServersToXML -FilePath $SubscriptionTemplate -Servers $Servers
+
+                Set-XML -FilePath $SubscriptionTemplate -Path 'Subscription' -Node 'SubscriptionId' -Value "PSWinReporting Subscription Events - $i"
+                Set-XML -FilePath $SubscriptionTemplate -Path 'Subscription' -Node 'ContentFormat' -Value 'Events'
+                Set-XML -FilePath $SubscriptionTemplate -Path 'Subscription' -Node 'ConfigurationMode' -Value 'Custom'
+                #$Events
+                Set-XML -FilePath $SubscriptionTemplate -Path 'Subscription' -Node 'Query' -Value $Events
+                Add-ToArray -List $ListTemplates -Element $SubscriptionTemplate
+            }
+
         }
-        Add-ToArray -List $Array -Element (New-EventQuery -Events $Systems -Type 'System')
-        $i = 0
-        foreach ($Events in $Array) {
-            $i++
-            $SubscriptionTemplate = "$ENV:TEMP\PSWinReportingSubscription$i.xml"
-            Copy-Item -Path $xmlTemplate $SubscriptionTemplate
-
-            Add-ServersToXML -FilePath $SubscriptionTemplate -Servers $Servers
-
-            Set-XML -FilePath $SubscriptionTemplate -Path 'Subscription' -Node 'SubscriptionId' -Value "PSWinReporting Subscription Events - $i"
-            Set-XML -FilePath $SubscriptionTemplate -Path 'Subscription' -Node 'ContentFormat' -Value 'Events'
-            Set-XML -FilePath $SubscriptionTemplate -Path 'Subscription' -Node 'ConfigurationMode' -Value 'Custom'
-            #$Events
-            Set-XML -FilePath $SubscriptionTemplate -Path 'Subscription' -Node 'Query' -Value $Events
-            Add-ToArray -List $ListTemplates -Element $SubscriptionTemplate
-        }
-
+    } else {
+        Write-Color 'Template not found ', $xmlTemplate -Color White, Yellow
     }
     return $ListTemplates
 }
