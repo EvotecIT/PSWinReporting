b41cf6d23d9a6d17cbc6e794f4876be9761f5db0
PrzemyslawKlys <pklys@evotec.pl>
2018-09-30 22:25:05 +0200

    Small cleanup

--- Private/Export-ReportToXlsX.ps1
+++ Private/Export-ReportToXlsX.ps1
@@ -1,9 +1,7 @@
 
-function Export-ReportToXLSX ($Report, $ReportOptions, $ReportFilePath, $ReportName, $ReportTable) {
+function Export-ReportToXLSX {
+    param($Report, $ReportOptions, $ReportFilePath, $ReportName, $ReportTable)
     if (($Report -eq $true) -and ($($ReportTable | Measure-Object).Count -gt 0)) {
         $ReportTable | ConvertTo-Excel -Path $ReportFilePath -WorkSheetname $ReportName -AutoSize -FreezeTopRow -AutoFilter
-        return
-    } else {
-        return
     }
 }
