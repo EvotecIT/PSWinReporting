f0bcb67c8b0fe205a798acba7bdc97d12c64e355
PrzemyslawKlys <pklys@evotec.pl>
2018-11-01 17:36:26 +0100

    Update to PSM1

--- PSWinReporting.psm1
+++ PSWinReporting.psm1
@@ -14,3 +14,15 @@ Foreach ($import in @($Public + $Private)) {
 #Export-ModuleMember -Function $Public.Basename -Verbose
 #Export-ModuleMember -Function * #-Verbose # 'Start-ADReporting', 'Get-KerberosLogonEvents', 'Get-GroupPolicyChanges', 'Get-EventLogClearedLogs'
 Export-ModuleMember -Function 'Start-ADReporting', 'Start-Notifications', 'New-SubscriptionTemplates', 'Set-SubscriptionTemplates', 'Remove-TaskScheduledForwarder', 'Add-TaskScheduledForwarder', 'Start-SubscriptionService', 'Start-RescanEvents'
+
+[string] $ManifestFile = '{0}.psd1' -f (Get-Item $PSCommandPath).BaseName;
+$ManifestPathAndFile = Join-Path -Path $PSScriptRoot -ChildPath $ManifestFile;
+if ( Test-Path -Path $ManifestPathAndFile) {
+    $Manifest = (Get-Content -raw $ManifestPathAndFile) | Invoke-Expression;
+    foreach ( $ScriptToProcess in $Manifest.ScriptsToProcess) {
+        $ModuleToRemove = (Get-Item (Join-Path -Path $PSScriptRoot -ChildPath $ScriptToProcess)).BaseName;
+        if (Get-Module $ModuleToRemove) {
+            Remove-Module $ModuleToRemove;
+        }
+    }
+}
