2e27badcfc86a4fcda104e3987474b89ec563df0
PrzemyslawKlys <pklys@evotec.pl>
2018-12-01 20:54:50 +0100

    Update

--- Examples/RunMe-SearchEvents.ps1
+++ Examples/RunMe-SearchEvents.ps1
@@ -1,6 +1,6 @@
 Import-Module PSWinReporting -Force
 Import-Module PSSharedGoods -Force
 
-$Events = Find-ADEvents -Report EventsReboots -DatesRange CurrentDay -Server AD1, AD2
+$Events = Find-ADEvents -Report UserLogon -DatesRange CurrentDay -Server Evo1 -Verbose # AD1, AD2
 $Events | Format-Table -Property *
 #$Events[0] | Format-List -Property *
--- Private/Reports/Get-LogonEvents.ps1
+++ Private/Reports/Get-LogonEvents.ps1
@@ -1,5 +1,15 @@
-function Get-LogonEvents($Events, $IgnoreWords = '') {
+function Get-LogonEvents {
+    param(
+        [Array] $Events,
+        $IgnoreWords = ''
+    )
+    $EventsType = $Script:ReportDefinitions.ReportsAD.EventBased.UserLogon.LogName
+    $EventsNeeded = $Script:ReportDefinitions.ReportsAD.EventBased.UserLogon.Events
+    $EventsFound = Find-EventsNeeded -Events $Events -EventsNeeded $EventsNeeded -EventsType $EventsType
+    $EventsFound = Get-EventsTranslation -Events $EventsFound -EventsDefinition $Script:ReportDefinitions.ReportsAD.EventBased.UserLogon
+    return Find-EventsIgnored -Events $EventsFound -IgnoreWords $IgnoreWords | Sort-Object $Script:ReportDefinitions.ReportsAD.EventBased.UserLogon.SortBy
 
+    <#
     # 4624: An account was successfully logged on
     # 4634: An account was logged off
     # 4647: User initiated logoff
@@ -9,4 +19,5 @@ function Get-LogonEvents($Events, $IgnoreWords = '') {
     $EventsFound = Find-EventsNeeded -Events $Events -EventsNeeded $EventsNeeded -EventsType $EventsType
     $EventsFound = Find-EventsIgnored -Events $EventsFound -IgnoreWords $IgnoreWords
     return $EventsFound
+    #>
 }
