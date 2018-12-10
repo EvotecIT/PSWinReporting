7faccf766ff6d382b30fc5991ea892cfe7c751ca
PrzemyslawKlys <pklys@evotec.pl>
2018-12-10 10:27:01 +0100

    Cleanup

--- Private/Other/Get-AllRequiredEvents.ps1
+++ Private/Other/Get-AllRequiredEvents.ps1
@@ -13,14 +13,9 @@ function Get-AllRequiredEvents {
     if ($Count -ne 0) {
         if ($FilePath) {
             $MyEvents = Get-Events -Path $FilePath -DateFrom $Dates.DateFrom -DateTo $Dates.DateTo -EventID $Events -LogName $LogName -Verbose:$Verbose
-            # $MyEvents | Add-Member -MemberType NoteProperty -Name "GatheredFrom" -Value $FilePath -Force
-            # $MyEvents | Add-Member -MemberType NoteProperty -Name "GatheredLog" -Value $LogName -Force
             return $MyEvents
         } else {
-            #  $ListServers = $Servers -join ','
             $MyEvents = Get-Events -Server $Servers -DateFrom $Dates.DateFrom -DateTo $Dates.DateTo -EventID $Events -LogName $LogName -Verbose:$Verbose
-            #  $MyEvents | Add-Member -MemberType NoteProperty -Name "GatheredFrom" -Value $ListServers -Force
-            #  $MyEvents | Add-Member -MemberType NoteProperty -Name "GatheredLog" -Value $LogName -Force
             return $MyEvents
         }
     }
