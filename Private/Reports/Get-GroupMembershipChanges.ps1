1ff6e130d6b4a542c20822b1b3c4ffc2de11b2b3
PrzemyslawKlys <pklys@evotec.pl>
2018-12-10 10:28:31 +0100

    Changes

--- Private/Reports/Get-GroupMembershipChanges.ps1
+++ Private/Reports/Get-GroupMembershipChanges.ps1
@@ -7,5 +7,5 @@ function Get-GroupMembershipChanges {
     $EventsNeeded = $Script:ReportDefinitions.ReportsAD.EventBased.GroupMembershipChanges.Events
     $EventsFound = Find-EventsNeeded -Events $Events -EventsNeeded $EventsNeeded -EventsType $EventsType
     $EventsFound = Get-EventsTranslation -Events $EventsFound -EventsDefinition $Script:ReportDefinitions.ReportsAD.EventBased.GroupMembershipChanges
-    return Find-EventsIgnored -Events $EventsFound -IgnoreWords $IgnoreWords | Sort-Object $Script:ReportDefinitions.ReportsAD.EventBased.GroupMembershipChanges.SortBy
+    return Find-EventsIgnored -Events $EventsFound -IgnoreWords $IgnoreWords | Sort-Object $Script:ReportDefinitions.ReportsAD.EventBased.GroupMembershipChanges.SortBy -Descending:$Script:ReportDefinitions.ReportsAD.EventBased.GroupMembershipChanges.Descending
 }
