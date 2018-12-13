9cd78599ea8d48dbf370fcfb88e44d993b04bd4b
PrzemyslawKlys <pklys@evotec.pl>
2018-12-13 22:05:53 +0100

    Update

--- Examples/RunMe-SearchEvents.ps1
+++ Examples/RunMe-SearchEvents.ps1
@@ -2,5 +2,6 @@
 Import-Module PSWinReporting -Force
 Import-Module PSSharedGoods #-Force
 
-$Events = Find-ADEvents -Report UserChanges -DatesRange CurrentMonth -Server AD1, AD2
+$Events = Find-ADEvents -Report UserStatus -DatesRange CurrentMonth -Server AD1, AD2
 $Events | Format-Table -Property *
+#$Events | Out-GridView
