9b351e5c7bf523704d4f5c458c22ef5b0155613c
PrzemyslawKlys <pklys@evotec.pl>
2018-12-19 12:16:43 +0100

    Fix for https://github.com/EvotecIT/PSWinReporting/issues/34

--- Private/TestFunctions/Test-Key.ps1
+++ Private/TestFunctions/Test-Key.ps1
@@ -1,7 +1,8 @@
 function Test-Key () {
+    [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [System.Collections.IDictionary] $ConfigurationTable,
+        [Object] $ConfigurationTable,
         [Parameter(Mandatory = $true)]
         [string] $ConfigurationSection,
         [Parameter(Mandatory = $true)]
@@ -9,27 +10,37 @@ function Test-Key () {
         [string] $ValueType,
         [switch] $DisplayProgress
     )
-    if (-not $ConfigurationTable.Contains($ConfigurationKey)) {
-        if ($DisplayProgress) {
-            $Logger.AddErrorRecord("Parameter $ConfigurationKey in configuration of $ConfigurationSection doesn't exist")
+    Write-Verbose "Test-Key ConfigurationKey: $ConfigurationKey, ConfigurationSection: $ConfigurationSection, ValueType: $ValueType, DisplayProgress: $DisplayProgress"
+    if ($ConfigurationTable -is [System.Collections.IDictionary]) {
+        if (-not $ConfigurationTable.Contains($ConfigurationKey)) {
+            if ($DisplayProgress) {
+                $Logger.AddErrorRecord("Parameter $ConfigurationKey in configuration of $ConfigurationSection doesn't exists.")
+            }
+            return $false
         }
-        return $false
     }
-
+    #Write-Verbose "Test-Key - Continuing 1"
     if (-not $PSBoundParameters.ContainsKey('ValueType')) {
         if ($DisplayProgress) {
             $Logger.AddSuccessRecord("Parameter $ConfigurationKey in configuration of $ConfigurationSection exists")
         }
         return $true
     }
-
-    if (-not ($ConfigurationTable.$ConfigurationKey.GetType().Name -eq $ValueType)) {
+    #Write-Verbose "Test-Key - Continuing 2"
+    if ($null -ne $ConfigurationTable.$ConfigurationKey) {
+        if (-not ($ConfigurationTable.$ConfigurationKey.GetType().Name -eq $ValueType)) {
+            if ($DisplayProgress) {
+                $Logger.AddErrorRecord("Parameter $ConfigurationKey in configuration of $ConfigurationSection exists but the type of key is incorrect")
+            }
+            return $false
+        }
+    } else {
         if ($DisplayProgress) {
-            $Logger.AddErrorRecord("Parameter $ConfigurationKey in configuration of $ConfigurationSection exists but the type of key is incorrect")
+            $Logger.AddErrorRecord("Parameter $ConfigurationKey in configuration of $ConfigurationSection doesn't exists.")
         }
         return $false
     }
-
+    #Write-Verbose "Test-Key - Continuing 3"
     if ($DisplayProgress) {
         $Logger.AddSuccessRecord("Parameter $ConfigurationKey in configuration of $ConfigurationSection exists and correct")
     }
