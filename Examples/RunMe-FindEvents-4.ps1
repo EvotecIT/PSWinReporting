78846a63cbdac22695a125806cef9a129dc49ac4
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-05-05 23:18:43 +0200

    Update

---
+++ Examples/RunMe-FindEvents-4.ps1
@@ -0,0 +1,145 @@
+﻿Import-Module 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSWinReportingV2\PSWinReportingV2.psd1' -Force
+
+$Definitions = @{
+    ADOrganizationalUnitChangesDetailed = [ordered] @{
+        Enabled        = $true
+        OUEventsModify = @{
+            Enabled          = $true
+            Events           = 5136, 5137, 5139, 5141
+            LogName          = 'Security'
+            Filter           = @{
+                'ObjectClass' = 'organizationalUnit'
+            }
+            Functions        = @{
+                'OperationType' = 'ConvertFrom-OperationType'
+            }
+
+            Fields           = [ordered] @{
+                'Computer'                 = 'Domain Controller'
+                'Action'                   = 'Action'
+                'OperationType'            = 'Action Detail'
+                'Who'                      = 'Who'
+                'Date'                     = 'When'
+                'ObjectDN'                 = 'Organizational Unit'
+                'AttributeLDAPDisplayName' = 'Field Changed'
+                'AttributeValue'           = 'Field Value'
+                #'OldObjectDN'              = 'OldObjectDN'
+                #'NewObjectDN'              = 'NewObjectDN'
+                # Common Fields
+                'RecordID'                 = 'Record ID'
+                'ID'                       = 'Event ID'
+                'GatheredFrom'             = 'Gathered From'
+                'GatheredLogName'          = 'Gathered LogName'
+            }
+            Overwrite        = @{
+                'Action Detail#1' = 'Action', 'A directory service object was created.', 'Organizational Unit Created'
+                'Action Detail#2' = 'Action', 'A directory service object was deleted.', 'Organizational Unit Deleted'
+                'Action Detail#3' = 'Action', 'A directory service object was moved.', 'Organizational Unit Moved'
+                #'Organizational Unit' = 'Action', 'A directory service object was moved.', 'OldObjectDN'
+                #'Field Changed'       = 'Action', 'A directory service object was moved.', ''
+                #'Field Value'         = 'Action', 'A directory service object was moved.', 'NewObjectDN'
+            }
+            OverwriteByField = @{
+                'Organizational Unit' = 'Action', 'A directory service object was moved.', 'OldObjectDN'
+                #'Field Changed'       = 'Action', 'A directory service object was moved.', ''
+                'Field Value'         = 'Action', 'A directory service object was moved.', 'NewObjectDN'
+            }
+            SortBy           = 'Record ID'
+            Descending       = $false
+            IgnoreWords      = @{}
+        }
+    }
+}
+
+$Target = [ordered]@{
+    Servers           = [ordered] @{
+        Enabled = $true
+        # Server1 = @{ ComputerName = 'EVO1'; LogName = 'ForwardedEvents' }
+        Server2 = 'AD1' #, 'AD2'
+        #Server1 = 'ADConnect.ad.evotec.xyz'
+    }
+    DomainControllers = [ordered] @{
+        Enabled = $false
+    }
+    LocalFiles        = [ordered] @{
+        Enabled     = $false
+        Directories = [ordered] @{
+            #MyEvents = 'C:\MyEvents' #
+            #MyOtherEvent = 'C:\MyEvent1'
+        }
+        Files       = [ordered] @{
+            File1 = 'C:\MyEvents\Archive-Security-2018-09-14-22-13-07-710.evtx'
+        }
+    }
+}
+$Times = @{
+    # Report Per Hour
+    PastHour             = @{
+        Enabled = $false # if it's 23:22 it will report 22:00 till 23:00
+    }
+    CurrentHour          = @{
+        Enabled = $false # if it's 23:22 it will report 23:00 till 00:00
+    }
+    # Report Per Day
+    PastDay              = @{
+        Enabled = $false # if it's 1.04.2018 it will report 31.03.2018 00:00:00 till 01.04.2018 00:00:00
+    }
+    CurrentDay           = @{
+        Enabled = $true # if it's 1.04.2018 05:22 it will report 1.04.2018 00:00:00 till 01.04.2018 00:00:00
+    }
+    # Report Per Week
+    OnDay                = @{
+        Enabled = $false
+        Days    = 'Monday'#, 'Tuesday'
+    }
+    # Report Per Month
+    PastMonth            = @{
+        Enabled = $false # checks for 1st day of the month - won't run on any other day unless used force
+        Force   = $false  # if true - runs always ...
+    }
+    CurrentMonth         = @{
+        Enabled = $false
+    }
+
+    # Report Per Quarter
+    PastQuarter          = @{
+        Enabled = $false # checks for 1st day fo the quarter - won't run on any other day
+        Force   = $false # if true - runs always ...
+    }
+    CurrentQuarter       = @{
+        Enabled = $false
+    }
+    # Report Custom
+    CurrentDayMinusDayX  = @{
+        Enabled = $false
+        Days    = 7    # goes back X days and shows just 1 day
+    }
+    CurrentDayMinuxDaysX = @{
+        Enabled = $false
+        Days    = 3 # goes back X days and shows X number of days till Today
+    }
+    CustomDate           = @{
+        Enabled  = $false
+        DateFrom = get-date -Year 2018 -Month 03 -Day 19
+        DateTo   = get-date -Year 2018 -Month 03 -Day 23
+    }
+    Last3days            = @{
+        Enabled = $false
+    }
+    Last7days            = @{
+        Enabled = $false
+    }
+    Last14days           = @{
+        Enabled = $false
+    }
+    Everything           = @{
+        Enabled = $false
+    }
+}
+
+$Events1 = Find-Events -Definitions $Definitions -Times $Times -Target $Target
+$Events1.ADOrganizationalUnitChangesDetailed | Out-HtmlView
+$Events1 | ft -AutoSize
+
+#$Events.ADOrganizationalUnitChangesDetailed | Out-HtmlView
+#$Events[0]
