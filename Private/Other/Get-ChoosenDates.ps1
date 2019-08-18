190b670c8413f92f9546b966244426e89eeb4c37
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-18 20:50:13 +0200

    Small fixes

--- Private/Other/Get-ChoosenDates.ps1
+++ Private/Other/Get-ChoosenDates.ps1
@@ -7,26 +7,26 @@ function Get-ChoosenDates {
         # Report Per Hour
         if ($ReportTimes.Contains('PastHour') -and $ReportTimes.PastHour.Enabled) {
             $DatesPastHour = Find-DatesPastHour
-            if ($DatesPastHour -ne $null) {
+            if ($DatesPastHour) {
                 $DatesPastHour
             }
         }
         if ($ReportTimes.Contains('CurrentHour') -and $ReportTimes.CurrentHour.Enabled) {
             $DatesCurrentHour = Find-DatesCurrentHour
-            if ($DatesCurrentHour -ne $null) {
+            if ($DatesCurrentHour) {
                 $DatesCurrentHour
             }
         }
         # Report Per Day
         if ($ReportTimes.Contains('PastDay') -and $ReportTimes.PastDay.Enabled) {
             $DatesDayPrevious = Find-DatesDayPrevious
-            if ($DatesDayPrevious -ne $null) {
+            if ($DatesDayPrevious) {
                 $DatesDayPrevious
             }
         }
         if ($ReportTimes.Contains('CurrentDay') -and $ReportTimes.CurrentDay.Enabled) {
             $DatesDayToday = Find-DatesDayToday
-            if ($DatesDayToday -ne $null) {
+            if ($DatesDayToday) {
                 $DatesDayToday
             }
         }
@@ -34,7 +34,7 @@ function Get-ChoosenDates {
         if ($ReportTimes.Contains('OnDay') -and $ReportTimes.OnDay.Enabled) {
             foreach ($Day in $ReportTimes.OnDay.Days) {
                 $DatesReportOnDay = Find-DatesPastWeek $Day
-                if ($DatesReportOnDay -ne $null) {
+                if ($DatesReportOnDay) {
                     $DatesReportOnDay
                 }
             }
@@ -43,13 +43,13 @@ function Get-ChoosenDates {
         if ($ReportTimes.Contains('PastMonth') -and $ReportTimes.PastMonth.Enabled) {
             # Find-DatesMonthPast runs only on 1st of the month unless -Force is used
             $DatesMonthPrevious = Find-DatesMonthPast -Force $ReportTimes.PastMonth.Force
-            if ($DatesMonthPrevious -ne $null) {
+            if ($DatesMonthPrevious) {
                 $DatesMonthPrevious
             }
         }
         if ($ReportTimes.Contains('CurrentMonth') -and $ReportTimes.CurrentMonth.Enabled) {
             $DatesMonthCurrent = Find-DatesMonthCurrent
-            if ($DatesMonthCurrent -ne $null) {
+            if ($DatesMonthCurrent) {
                 $DatesMonthCurrent
             }
         }
@@ -57,26 +57,26 @@ function Get-ChoosenDates {
         if ($ReportTimes.Contains('PastQuarter') -and $ReportTimes.PastQuarter.Enabled) {
             # Find-DatesMonthPast runs only on 1st of the quarter unless -Force is used
             $DatesQuarterLast = Find-DatesQuarterLast -Force $ReportTimes.PastQuarter.Force
-            if ($DatesQuarterLast -ne $null) {
+            if ($DatesQuarterLast) {
                 $DatesQuarterLast
             }
         }
         if ($ReportTimes.Contains('CurrentQuarter') -and $ReportTimes.CurrentQuarter.Enabled) {
             $DatesQuarterCurrent = Find-DatesQuarterCurrent
-            if ($DatesQuarterCurrent -ne $null) {
+            if ($DatesQuarterCurrent) {
                 $DatesQuarterCurrent
             }
         }
         # Report Custom
         if ($ReportTimes.Contains('CurrentDayMinusDayX') -and $ReportTimes.CurrentDayMinusDayX.Enabled) {
             $DatesCurrentDayMinusDayX = Find-DatesCurrentDayMinusDayX $ReportTimes.CurrentDayMinusDayX.Days
-            if ($DatesCurrentDayMinusDayX -ne $null) {
+            if ($DatesCurrentDayMinusDayX) {
                 $DatesCurrentDayMinusDayX
             }
         }
         if ($ReportTimes.Contains('CurrentDayMinuxDaysX') -and $ReportTimes.CurrentDayMinuxDaysX.Enabled) {
             $DatesCurrentDayMinusDaysX = Find-DatesCurrentDayMinuxDaysX $ReportTimes.CurrentDayMinuxDaysX.Days
-            if ($DatesCurrentDayMinusDaysX -ne $null) {
+            if ($DatesCurrentDayMinusDaysX) {
                 $DatesCurrentDayMinusDaysX
             }
         }
@@ -85,7 +85,7 @@ function Get-ChoosenDates {
                 DateFrom = $ReportTimes.CustomDate.DateFrom
                 DateTo   = $ReportTimes.CustomDate.DateTo
             }
-            if ($DatesCustom -ne $null) {
+            if ($DatesCustom) {
                 $DatesCustom
             }
         }
@@ -98,19 +98,19 @@ function Get-ChoosenDates {
         }
         if ($ReportTimes.Contains('Last3days') -and $ReportTimes.Last3days.Enabled) {
             $DatesCurrentDayMinusDaysX = Find-DatesCurrentDayMinuxDaysX -days 3
-            if ($DatesCurrentDayMinusDaysX -ne $null) {
+            if ($DatesCurrentDayMinusDaysX) {
                 $DatesCurrentDayMinusDaysX
             }
         }
         if ($ReportTimes.Contains('Last7days') -and $ReportTimes.Last7days.Enabled) {
             $DatesCurrentDayMinusDaysX = Find-DatesCurrentDayMinuxDaysX -days 7
-            if ($DatesCurrentDayMinusDaysX -ne $null) {
+            if ($DatesCurrentDayMinusDaysX) {
                 $DatesCurrentDayMinusDaysX
             }
         }
         if ($ReportTimes.Contains('Last14days') -and $ReportTimes.Last14days.Enabled) {
             $DatesCurrentDayMinusDaysX = Find-DatesCurrentDayMinuxDaysX -days 14
-            if ($DatesCurrentDayMinusDaysX -ne $null) {
+            if ($DatesCurrentDayMinusDaysX) {
                 $DatesCurrentDayMinusDaysX
             }
         }
