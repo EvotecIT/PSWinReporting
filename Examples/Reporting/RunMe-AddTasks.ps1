5253dc9872828b91c625e22d551fd0ca769da612
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-20 00:14:59 +0200

    Update

---
+++ Examples/Reporting/RunMe-AddTasks.ps1
@@ -0,0 +1,12 @@
+Import-Module .\PSWinReportingV2.psd1 -Force
+Import-Module PSSharedGoods
+
+$TaskName = 'ForwardedEvents'
+$TaskPath = '\Event Viewer Tasks\'
+$Author = 'EVOTEC'
+$URI = '\Event Viewer Tasks\ForwardedEvents'
+$Command = 'powershell.exe'
+$Argument = @('-windowstyle hidden', "$PSScriptRoot\RunMe-TriggerOnEvents.ps1", '-EventID $(eventID) -eventRecordID $(eventRecordID) -eventChannel $(eventChannel) -eventSeverity $(eventSeverity)')
+
+Remove-WinTaskScheduledForwarder -TaskPath $TaskPath -TaskName $TaskName
+Add-WinTaskScheduledForwarder -TaskPath $TaskPath -TaskName $TaskName -Author $Author -URI $Uri -Command $Command -Argument $Argument
