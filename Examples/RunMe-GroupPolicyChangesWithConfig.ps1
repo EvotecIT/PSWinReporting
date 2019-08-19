f9b25541726884f5603e33ff187cf142601eb901
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-20 00:13:31 +0200

    small update to example

---
+++ Examples/RunMe-GroupPolicyChangesWithConfig.ps1
@@ -0,0 +1,246 @@
+﻿Import-Module .\PSWinReportingV2.psd1 -Force
+
+$Definitions = @{
+    ADGroupPolicyChanges = [ordered] @{
+        Enabled                     = $true
+        'Group Policy Name Changes' = @{
+            Enabled     = $true
+            Events      = 5136, 5137, 5141
+            LogName     = 'Security'
+            Filter      = @{
+                # Filter is special, if there is just one object on the right side
+                # If there are more objects filter will pick all values on the right side and display them as required
+                'ObjectClass'              = 'groupPolicyContainer'
+                #'OperationType'            = 'Value Added'
+                'AttributeLDAPDisplayName' = $null, 'displayName' #, 'versionNumber'
+            }
+            Functions   = @{
+                'OperationType' = 'ConvertFrom-OperationType'
+            }
+            Fields      = [ordered] @{
+                'RecordID'                 = 'Record ID'
+                'Computer'                 = 'Domain Controller'
+                'Action'                   = 'Action'
+                'Who'                      = 'Who'
+                'Date'                     = 'When'
+
+
+                'ObjectDN'                 = 'ObjectDN'
+                'ObjectGUID'               = 'ObjectGUID'
+                'ObjectClass'              = 'ObjectClass'
+                'AttributeLDAPDisplayName' = 'AttributeLDAPDisplayName'
+                #'AttributeSyntaxOID'       = 'AttributeSyntaxOID'
+                'AttributeValue'           = 'AttributeValue'
+                'OperationType'            = 'OperationType'
+                'OpCorrelationID'          = 'OperationCorelationID'
+                'AppCorrelationID'         = 'OperationApplicationCorrelationID'
+
+                'DSName'                   = 'DSName'
+                'DSType'                   = 'DSType'
+                'Task'                     = 'Task'
+                'Version'                  = 'Version'
+
+                # Common Fields
+                'ID'                       = 'Event ID'
+
+                'GatheredFrom'             = 'Gathered From'
+                'GatheredLogName'          = 'Gathered LogName'
+            }
+
+            SortBy      = 'Record ID'
+            Descending  = $false
+            IgnoreWords = @{
+
+            }
+        }
+        'Group Policy Edits'        = @{
+            Enabled     = $true
+            Events      = 5136, 5137, 5141
+            LogName     = 'Security'
+            Filter      = @{
+                # Filter is special, if there is just one object on the right side
+                # If there are more objects filter will pick all values on the right side and display them as required
+                'ObjectClass'              = 'groupPolicyContainer'
+                #'OperationType'            = 'Value Added'
+                'AttributeLDAPDisplayName' = 'versionNumber'
+            }
+            Functions   = @{
+                'OperationType' = 'ConvertFrom-OperationType'
+            }
+            Fields      = [ordered] @{
+                'RecordID'                 = 'Record ID'
+                'Computer'                 = 'Domain Controller'
+                'Action'                   = 'Action'
+                'Who'                      = 'Who'
+                'Date'                     = 'When'
+
+
+                'ObjectDN'                 = 'ObjectDN'
+                'ObjectGUID'               = 'ObjectGUID'
+                'ObjectClass'              = 'ObjectClass'
+                'AttributeLDAPDisplayName' = 'AttributeLDAPDisplayName'
+                #'AttributeSyntaxOID'       = 'AttributeSyntaxOID'
+                'AttributeValue'           = 'AttributeValue'
+                'OperationType'            = 'OperationType'
+                'OpCorrelationID'          = 'OperationCorelationID'
+                'AppCorrelationID'         = 'OperationApplicationCorrelationID'
+
+                'DSName'                   = 'DSName'
+                'DSType'                   = 'DSType'
+                'Task'                     = 'Task'
+                'Version'                  = 'Version'
+
+                # Common Fields
+                'ID'                       = 'Event ID'
+
+                'GatheredFrom'             = 'Gathered From'
+                'GatheredLogName'          = 'Gathered LogName'
+            }
+
+            SortBy      = 'Record ID'
+            Descending  = $false
+            IgnoreWords = @{
+
+            }
+        }
+        'Group Policy Links'        = @{
+            Enabled     = $true
+            Events      = 5136, 5137, 5141
+            LogName     = 'Security'
+            Filter      = @{
+                # Filter is special, if there is just one object on the right side
+                # If there are more objects filter will pick all values on the right side and display them as required
+                'ObjectClass' = 'domainDNS'
+                #'OperationType'            = 'Value Added'
+                #'AttributeLDAPDisplayName' = 'versionNumber'
+            }
+            Functions   = @{
+                'OperationType' = 'ConvertFrom-OperationType'
+            }
+            Fields      = [ordered] @{
+                'RecordID'                 = 'Record ID'
+                'Computer'                 = 'Domain Controller'
+                'Action'                   = 'Action'
+                'Who'                      = 'Who'
+                'Date'                     = 'When'
+
+
+                'ObjectDN'                 = 'ObjectDN'
+                'ObjectGUID'               = 'ObjectGUID'
+                'ObjectClass'              = 'ObjectClass'
+                'AttributeLDAPDisplayName' = 'AttributeLDAPDisplayName'
+                #'AttributeSyntaxOID'       = 'AttributeSyntaxOID'
+                'AttributeValue'           = 'AttributeValue'
+                'OperationType'            = 'OperationType'
+                'OpCorrelationID'          = 'OperationCorelationID'
+                'AppCorrelationID'         = 'OperationApplicationCorrelationID'
+
+                'DSName'                   = 'DSName'
+                'DSType'                   = 'DSType'
+                'Task'                     = 'Task'
+                'Version'                  = 'Version'
+
+                # Common Fields
+                'ID'                       = 'Event ID'
+
+                'GatheredFrom'             = 'Gathered From'
+                'GatheredLogName'          = 'Gathered LogName'
+            }
+
+            SortBy      = 'Record ID'
+            Descending  = $false
+            IgnoreWords = @{
+
+            }
+        }
+    }
+}
+
+$Target = [ordered]@{
+    Servers           = [ordered] @{
+        Enabled = $true
+        # Server1 = @{ ComputerName = 'EVO1'; LogName = 'ForwardedEvents' }
+        Server2 = 'AD1', 'AD2'
+        #Server1 = 'ADConnect.ad.evotec.xyz'
+    }
+    DomainControllers = [ordered] @{
+        Enabled = $false
+    }
+    LocalFiles        = [ordered] @{
+        Enabled     = $false
+        Directories = [ordered] @{
+            #MyEvents = 'C:\MyEvents' #
+            #MyOtherEvent = 'C:\MyEvent1'
+        }
+        Files       = [ordered] @{
+            File1 = 'C:\MyEvents\Archive-Security-2018-09-14-22-13-07-710.evtx'
+        }
+    }
+}
+$Times = @{
+    # Report Per Hour
+    PastHour             = @{
+        Enabled = $false # if it's 23:22 it will report 22:00 till 23:00
+    }
+    CurrentHour          = @{
+        Enabled = $false # if it's 23:22 it will report 23:00 till 00:00
+    }
+    # Report Per Day
+    PastDay              = @{
+        Enabled = $false # if it's 1.04.2018 it will report 31.03.2018 00:00:00 till 01.04.2018 00:00:00
+    }
+    CurrentDay           = @{
+        Enabled = $true # if it's 1.04.2018 05:22 it will report 1.04.2018 00:00:00 till 01.04.2018 00:00:00
+    }
+    # Report Per Week
+    OnDay                = @{
+        Enabled = $false
+        Days    = 'Monday'#, 'Tuesday'
+    }
+    # Report Per Month
+    PastMonth            = @{
+        Enabled = $false # checks for 1st day of the month - won't run on any other day unless used force
+        Force   = $false  # if true - runs always ...
+    }
+    CurrentMonth         = @{
+        Enabled = $false
+    }
+
+    # Report Per Quarter
+    PastQuarter          = @{
+        Enabled = $false # checks for 1st day fo the quarter - won't run on any other day
+        Force   = $false # if true - runs always ...
+    }
+    CurrentQuarter       = @{
+        Enabled = $false
+    }
+    # Report Custom
+    CurrentDayMinusDayX  = @{
+        Enabled = $false
+        Days    = 7    # goes back X days and shows just 1 day
+    }
+    CurrentDayMinuxDaysX = @{
+        Enabled = $false
+        Days    = 3 # goes back X days and shows X number of days till Today
+    }
+    CustomDate           = @{
+        Enabled  = $false
+        DateFrom = get-date -Year 2018 -Month 03 -Day 19
+        DateTo   = get-date -Year 2018 -Month 03 -Day 23
+    }
+    Last3days            = @{
+        Enabled = $false
+    }
+    Last7days            = @{
+        Enabled = $false
+    }
+    Last14days           = @{
+        Enabled = $false
+    }
+    Everything           = @{
+        Enabled = $false
+    }
+}
+
+$Events = Find-Events -Definitions $Definitions -Times $Times -Target $Target
+$Events
