faaaba31a1959ee29889515213b767766f0b07ed
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-20 00:14:48 +0200

    Update

--- Examples/RunMe-GroupPolicyChanges.ps1
+++
@@ -1,4 +0,0 @@
-﻿Import-Module .\PSWinReportingV2.psd1 -Force
-
-$Events = Find-Events -Report ADGroupPolicyChanges,ADOrganizationalUnitChangesDetailed -DatesRange CurrentDay -Servers 'AD1' -Verbose
-$Events | Format-Table -AutoSize
