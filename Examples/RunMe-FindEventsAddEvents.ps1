9ca5e4defc4da5f25e743c25c2346af0b0c3d697
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-20 00:10:42 +0200

    Update

--- Examples/RunMe-FindEventsAddEvents.ps1
+++ Examples/RunMe-FindEventsAddEvents.ps1
@@ -1,9 +1,4 @@
 Import-Module .\PSWinReportingV2.psd1 -Force
-#Import-Module PSEventViewer -Force
-#Import-Module PSSharedGoods -Force
-
-#Find-Events -Report UserChanges -DatesRange Last14days -Servers 'AD1','AD2' -Verbose
-
 
 $DefinitionsADSync = @{
     AzureSynchronizationErrors  = @{
