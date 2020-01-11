952ba6e852ad79e88d47ea4e4a72fc9114cd81fc
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-01-11 14:29:47 +0100

    Saving new version before removal

--- Private/Other/New-EventQuery.ps1
+++ Private/Other/New-EventQuery.ps1
@@ -14,16 +14,24 @@ function New-EventQuery {
                 ]]>
     #>
     Write-Verbose "New-EventQuery - Events Count: $($Events.Count)"
-    $values = New-ArrayList
-    Add-ToArray -List $Values -Element '<QueryList><Query Id="0" Path="Security">'
-    Add-ToArray -List $Values -Element "<Select Path=`"$Type`">*[System[("
-    foreach ($E in $Events) {
-        Add-ToArray -List $Values -Element "EventID=$E"
-        Add-ToArray -List $Values -Element "or"
-    }
-    Remove-FromArray -List $values -LastElement
-    Add-ToArray -List $Values -Element ')]]</Select></Query></QueryList>'
+    $values = @(
+        '<QueryList><Query Id="0" Path="Security">'
+        "<Select Path=`"$Type`">*[System[("
+        for ($i = 0; $i -lt $Events.Count; $i++) {
+            "EventID=$($Events[$i])"
+            if ($i -lt $Events.Count - 1) {
+                'or'
+            }
+        }
+        $Output
+        ')]]</Select></Query></QueryList>'
+    )
     $FinalQuery = ([string] $Values)
     Write-Verbose $FinalQuery
     return ([string] $Values)
 }
+<#
+<QueryList><Query Id="0" Path="Security"> <Select Path="">*[System[( EventID=115 )]]</Select></Query></QueryList>
+#>
+
+#New-EventQuery -Events '115', 116 -Verbose
