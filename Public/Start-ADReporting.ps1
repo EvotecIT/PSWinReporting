8ddbecc31ae584bd8bc5f6f484138976ff432dad
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-25 20:35:45 +0200

    Fixes #62 Script will now exit if wrong log path is provided

--- Public/Start-ADReporting.ps1
+++ Public/Start-ADReporting.ps1
@@ -8,9 +8,18 @@ function Start-ADReporting () {
     )
     Set-DisplayParameters -ReportOptions $ReportOptions
 
+    if ($ReportOptions.DisplayConsole -and $ReportOptions.DisplayConsole.LogFile) {
+        $ReportPath = [io.path]::GetDirectoryName($ReportOptions.DisplayConsole.LogFile)
+        if (-not (Test-Path -LiteralPath $ReportPath)) {
+            Write-Color -Text '[i] ', "LogFile path doesn't exists ", $ReportPath, ". Please fix log path or provide an empty string. Current log file: ", $ReportOptions.DisplayConsole.LogFile -Color White, White, Yellow, White, Yellow
+            return
+            #$null = New-Item -Path $ReportPath -ItemType Directory -Force
+        }
+    }
+
     Test-Prerequisite $EmailParameters $FormattingParameters $ReportOptions $ReportTimes $ReportDefinitions
     if ($null -ne $ReportOptions.JustTestPrerequisite -and $ReportOptions.JustTestPrerequisite -eq $true) {
-        Exit
+        return
     }
 
     ## Added for compatibility reasons
@@ -20,13 +29,13 @@ function Start-ADReporting () {
     if (-not $ReportOptions.Contains('SendMailOnlyOnEvents')) {
         $ReportOptions.SendMailOnlyOnEvents = $false
     }
-    
+
     if (-not $ReportDefinitions.ReportsAD.Servers.Contains('UseDirectScan')) {
         if ($ReportOptions.ReportsAD.Servers.UseForwarders) {
             $ReportDefinitions.ReportsAD.Servers.UseDirectScan = $false
         } else {
             $ReportDefinitions.ReportsAD.Servers.UseDirectScan = $true
-        }        
+        }
     }
     if (-not $ReportDefinitions.ReportsAD.Servers.Contains('UseForwarders')) {
         $ReportDefinitions.ReportsAD.Servers.UseDirectScan = $true
