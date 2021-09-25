bbfd122701f8383f2e18138d6887b57f619bd8c9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-09-25 21:01:21 +0200

    Fixes #60 It now should not throw errors It should report problems instead

--- Private/Get-EventLogSize.ps1
+++ Private/Get-EventLogSize.ps1
@@ -1,14 +1,12 @@
 function Get-EventLogSize {
     [CmdletBinding()]
     param(
-        $Servers,
-        $LogName = "Security"
+        [Array] $Servers,
+        [string] $LogName = "Security"
     )
-    $Verbose = ($PSCmdlet.MyInvocation.BoundParameters['Verbose'] -eq $true)
-    $results = @()
-    foreach ($server in $Servers) {
+    [Array] $results = foreach ($server in $Servers) {
         try {
-            $result = Get-WinEvent -ListLog $LogName -ComputerName $server | Select-Object MaximumSizeInBytes, FileSize, IsLogFul, LastAccessTime, LastWriteTime, OldestRecordNumber, RecordCount, LogName, LogType, LogIsolation, IsEnabled, LogMode
+            $result = Get-WinEvent -ListLog $LogName -ComputerName $server -ErrorAction Stop | Select-Object MaximumSizeInBytes, FileSize, IsLogFul, LastAccessTime, LastWriteTime, OldestRecordNumber, RecordCount, LogName, LogType, LogIsolation, IsEnabled, LogMode
         } catch {
             $ErrorMessage = $_.Exception.Message -replace "`n", " " -replace "`r", " "
             Write-Color @script:WriteParameters "[-] ", "Event Log Error on ", $Server, ': ', $ErrorMessage -Color White, White, Yellow, White, Red
@@ -16,15 +14,24 @@ function Get-EventLogSize {
         }
         $CurrentFileSize = Convert-Size -Value $($result.FileSize) -From Bytes -To GB -Precision 2 -Display
         $MaximumFilesize = Convert-Size -Value $($result.MaximumSizeInBytes) -From Bytes -To GB -Precision 2 -Display
-        $EventOldest = (Get-WinEvent -MaxEvents 1 -LogName $result.LogName -Oldest -ComputerName $Server).TimeCreated
-        $EventNewest = (Get-WinEvent -MaxEvents 1 -LogName $result.LogName -ComputerName $Server).TimeCreated
+        try {
+            $EventOldest = (Get-WinEvent -MaxEvents 1 -LogName $result.LogName -Oldest -ComputerName $Server -ErrorAction Stop).TimeCreated
+        } catch {
+            $ErrorMessage = $_.Exception.Message -replace "`n", " " -replace "`r", " "
+            Write-Color @script:WriteParameters "[-] ", "Couldn't get single event from ", $Server, ': ', "from log ", $result.LogName, " (oldest). Error message:", $ErrorMessage -Color White, White, Yellow, White, Red
+        }
+        try {
+            $ErrorMessage = $_.Exception.Message -replace "`n", " " -replace "`r", " "
+            $EventNewest = (Get-WinEvent -MaxEvents 1 -LogName $result.LogName -ComputerName $Server -ErrorAction Stop).TimeCreated
+        } catch {
+            Write-Color @script:WriteParameters "[-] ", "Couldn't get single event from ", $Server, ': ', "from log ", $result.LogName, " (newest). Error message:", $ErrorMessage -Color White, White, Yellow, White, Red
+        }
         Add-Member -InputObject $result -MemberType NoteProperty -Name "Server" -Value $server
         Add-Member -InputObject $result -MemberType NoteProperty -Name "CurrentFileSize" -Value $CurrentFileSize
         Add-Member -InputObject $result -MemberType NoteProperty -Name "MaximumFileSize" -Value $MaximumFilesize
         Add-Member -InputObject $result -MemberType NoteProperty -Name "EventOldest" -Value $EventOldest
         Add-Member -InputObject $result -MemberType NoteProperty -Name "EventNewest" -Value $EventNewest
-        $results += $result
+        $result
     }
-
-    return $results | Select-Object Server, LogName, LogType, EventOldest, EventNewest, "CurrentFileSize", "MaximumFileSize", LogMode, IsEnabled
+    $results | Select-Object Server, LogName, LogType, EventOldest, EventNewest, "CurrentFileSize", "MaximumFileSize", LogMode, IsEnabled
 }
